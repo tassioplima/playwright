4e8b17811d4e45ac836c4c854a67f027ace3e549
Tassio Lima <tassiolima@Tassios-MBP.lan>
2024-01-16 19:58:05 +0000

    update test

--- tests/amazonHome.spec.js
+++ tests/amazonHome.spec.js
@@ -2,25 +2,25 @@
 const { test, expect } = require('@playwright/test');
 const { AmazonHomePage } = require("../pages/homePageAmazon")
 
-const homePage = new AmazonHomePage()
+const HomePage = new AmazonHomePage()
 
-test(`Successfully Search for ${homePage.ObjectsPage.ProductTarget} on Amazon`, async ({ page }) => {
+test(`Successfully Search for ${HomePage.ObjectsPage.ProductTarget} on Amazon`, async ({ page }) => {
   global.page = page
 
   await test.step('Navigate to the main screen of amazon.com', async () => {
-    await homePage.navegatTo();
+    await HomePage.navegatTo();
   })
 
   await test.step('Main screen of Amazon is displayed', async () => {
-    await homePage.validateHomePage()
+    await HomePage.validateHomePage()
   })
   
-  await test.step(`Search for the product  ${homePage.ObjectsPage.ProductTarget}`, async () => {
-    await homePage.validateSearchProduct(homePage.ObjectsPage.ProductTarget)
+  await test.step(`Search for the product  ${HomePage.ObjectsPage.ProductTarget}`, async () => {
+    await HomePage.validateSearchProduct(HomePage.ObjectsPage.ProductTarget)
   })
 
   await test.step(`Validate the found product`, async () => {
-    await homePage.validateSearchProduct(homePage.ObjectsPage.ProductTarget)
+    await HomePage.validateSearchProduct(HomePage.ObjectsPage.ProductTarget)
   })
 
 });
